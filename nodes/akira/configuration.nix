015d123a355095b50c6477ec2887631cf7055c05
Kita Lembrik <kita@lembrik.eu>
2026-04-22 02:32:33 +0400

    win11 vm

--- nodes/akira/configuration.nix
+++ nodes/akira/configuration.nix
@@ -242,6 +242,9 @@
       RUN+="${pkgs.hdparm}/bin/hdparm -S 180 /dev/%k"
   '';
 
+  # Hardware OpenGL (Mesa) for VM 3D Acceleration
+  hardware.opengl.enable = true;
+
   # Tailscale
   services.tailscale.enable = true;
 
@@ -276,8 +279,61 @@
     htpasswd-file = "/media/restic/.htpasswd";
   };
 
-  networking.firewall.allowedTCPPorts = [ 445 8000 8081 8083 ];
+  networking.firewall.allowedTCPPorts = [ 445 8000 8081 8083 946 ];
   networking.firewall.allowedUDPPorts = [ 5353 51820 ];  # mDNS, WireGuard
+
+  # Windows 11 VM
+  systemd.services.swtpm-win11 = {
+    description = "SWTPM for Windows 11 VM";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /var/lib/vm/win11/swtpm";
+      ExecStart = "${pkgs.swtpm}/bin/swtpm socket --tpmstate dir=/var/lib/vm/win11/swtpm --ctrl type=unixio,path=/var/lib/vm/win11/swtpm-sock --tpm2";
+      Restart = "on-failure";
+    };
+  };
+
+  systemd.services.win11-vm = {
+    description = "Windows 11 QEMU VM";
+    wants = [ "swtpm-win11.service" ];
+    after = [ "swtpm-win11.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStartPre = [
+        "${pkgs.coreutils}/bin/mkdir -p /var/lib/vm/win11"
+        "${pkgs.bash}/bin/sh -c 'if [ ! -f /var/lib/vm/win11/OVMF_VARS.fd ]; then ${pkgs.coreutils}/bin/cp --no-preserve=mode ${pkgs.OVMFFull.fd}/FV/OVMF_VARS.fd /var/lib/vm/win11/OVMF_VARS.fd; fi'"
+        "${pkgs.bash}/bin/sh -c 'if [ ! -f /var/lib/vm/win11/disk.qcow2 ]; then ${pkgs.qemu_kvm}/bin/qemu-img create -f qcow2 /var/lib/vm/win11/disk.qcow2 120G; fi'"
+      ];
+      ExecStart = ''
+        ${pkgs.qemu_kvm}/bin/qemu-system-x86_64 -name win11 -enable-kvm -m 6G -smp 4 \
+          -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time -machine q35 \
+          -device virtio-vga-gl -display egl-headless,rendernode=/dev/dri/renderD128 \
+          -chardev socket,id=chrtpm,path=/var/lib/vm/win11/swtpm-sock \
+          -tpmdev emulator,id=tpm0,chardev=chrtpm \
+          -device tpm-tis,tpmdev=tpm0 \
+          -drive if=pflash,format=raw,readonly=on,file=${pkgs.OVMFFull.fd}/FV/OVMF_CODE.fd \
+          -drive if=pflash,format=raw,file=/var/lib/vm/win11/OVMF_VARS.fd \
+          -drive file=/var/lib/vm/win11/disk.qcow2,format=qcow2,if=virtio \
+          -netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
+          -device usb-ehci,id=ehci -device usb-tablet \
+          -vnc 127.0.0.1:1
+      '';
+      Restart = "on-failure";
+    };
+  };
+
+  # Proxy VNC to port 946
+  systemd.services.win11-vnc-proxy = {
+    description = "VNC Proxy for Windows 11 VM on port 946";
+    requires = [ "win11-vm.service" ];
+    after = [ "win11-vm.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.socat}/bin/socat TCP-LISTEN:946,fork,reuseaddr TCP:127.0.0.1:5901";
+      Restart = "always";
+      RestartSec = 3;
+    };
+  };
 
   # State version
   system.stateVersion = "23.05";
