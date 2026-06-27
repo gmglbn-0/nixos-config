b4df9524ff78694f93d8715d679b848410e2f031
Kita Lembrik <kita@lembrik.eu>
2026-06-27 05:02:41 +0400

    bwa

--- nodes/akira/hardware-configuration.nix
+++ nodes/akira/hardware-configuration.nix
@@ -12,19 +12,15 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/392c00a1-9d5c-41a8-ab99-492efefe900d";
+    { device = "/dev/disk/by-uuid/2b4058a8-d291-4144-b720-cb27e8fe713b";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/3CF9-1475";
+    { device = "/dev/disk/by-uuid/D7FA-A226";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/fa68f11c-0d4c-472d-af0c-495f0ef5abb4"; }
-    ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
