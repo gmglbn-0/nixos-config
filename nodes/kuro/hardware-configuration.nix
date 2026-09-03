c7ed61b7540a09d1e73525202b98ce860601bcde
Kita Lembrik <kita@lembrik.eu>
2026-09-04 02:26:52 +0400

    fix: hardware of kuro

--- nodes/kuro/hardware-configuration.nix
+++ nodes/kuro/hardware-configuration.nix
@@ -5,18 +5,18 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" "i915" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" "i915" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos";
+    { device = "/dev/disk/by-uuid/ddc9289e-ec7d-415d-b14d-579a09a78bda";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-label/boot";
+    { device = "/dev/disk/by-uuid/37A9-4B23";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
